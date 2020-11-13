404d9a39480935515f0abd6f111f36c25af03ef0
victorurchella <victorhugourchella@hotmail.com>
2020-11-13 14:25:28 -0300

    commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+game 'gta5'
+
+ui_page 'ui/index.html'
+files {
+  'ui/index.html',
+  'ui/style.css',
+  'ui/img/logo.png',
+  'ui/script.js'
+}
+
+client_script "client.lua"
